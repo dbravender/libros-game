a5748af69284a46c58b4174e832ded892944f483
Dan Bravender <dan.bravender@gmail.com>
2015-02-13 19:33:57 +0900

    Card selection for bidding

--- html/game.js
+++ html/game.js
@@ -12,12 +12,20 @@ var GameView = React.createClass({
       }
       cardsByType[type].push(card);
     }
-    var childNodes = [];
+    this.childNodes = [];
     for (var type in cardsByType) {
       var cards = cardsByType[type];
-      childNodes.push(<li key={type}><CardList cards={cards}/></li>);
+      this.childNodes.push(<CardList cards={cards}/>);
     }
-    return <ul>{childNodes}</ul>;
+    return <ul>{this.childNodes}</ul>;
+  },
+  selectedCards: function () {
+    var childNodeSelectedCards = [];
+    for (var i in this.childNodes) {
+      var childNode = this.childNodes[i];
+      childNodeSelectedCards.push(childNode.selectedCards());
+    }
+    return [].concat.apply([], childNodeSelectedCards);
   }
 });
 
@@ -26,7 +34,10 @@ var Card = React.createClass({
     return {selected: false};
   },
   render: function () {
-    return <div key={this.props.card.id} className={'card ' + this.props.card.type + '-type'}>{this.props.card.value} {this.props.card.letter}</div>
+    return <div onClick={this.toggle} key={this.props.card.id} className={(this.state.selected ? 'selected' : '') + ' card ' + this.props.card.type + '-type'}>{this.props.card.value} {this.props.card.letter}</div>
+  },
+  toggle: function() {
+    this.setState({selected: !this.state.selected})
   }
 });
 
@@ -39,41 +50,75 @@ var CardList = React.createClass({
     var lowestLetter = this.props.cards.reduce(function(x, y) {
       return [x, y.letter].sort()[0];
     }, null);
-    var cardNodes = this.props.cards.map(function(card) {
+    this.cardNodes = this.props.cards.map(function(card) {
       return <Card key={card.id} card={card} />;
     });
     return (
-      <div className="card-list">
+      <li className="card-list">
         <span className={'card-summary badge ' + this.props.cards[0].type + '-type'}>
           {totalValue} {lowestLetter}
         </span>
-        {' '}{cardNodes}
-      </div>
+        {' '}{this.cardNodes}
+      </li>
     );
+  },
+  selectedCards: function () {
+    return this.cardNodes
+               .filter(function (cardNode) { return cardNode.state.selected; })
+               .map(function (cardNode) { return cardNode.props.card.id; })
   }
 });
 
 var cards = [
-  {value: 1, type: 'red', letter: 'A', 'id': 0},
-  {value: 1, type: 'red', letter: 'B', 'id': 1},
-  {value: 1, type: 'red', letter: 'C', 'id': 2},
-  {value: 1, type: 'red', letter: 'C', 'id': 3},
-  {value: 1, type: 'red', letter: 'C', 'id': 4},
-  {value: 1, type: 'red', letter: 'C', 'id': 5},
-  {value: 1, type: 'red', letter: 'C', 'id': 6},
-  {value: 1, type: 'red', letter: 'C', 'id': 7},
-  {value: 1, type: 'red', letter: 'C', 'id': 8},
-  {value: 1, type: 'red', letter: 'C', 'id': 9},
-  {value: 1, type: 'red', letter: 'C', 'id': 10},
-  {value: 1, type: 'red', letter: 'C', 'id': 11},
-  {value: 1, type: 'red', letter: 'C', 'id': 12},
-  {value: 1, type: 'red', letter: 'C', 'id': 13},
-  {value: 2, type: 'blue', letter: 'B', 'id': 14},
-  {value: 1, type: 'brown', letter: 'A', 'id': 15},
-  {value: 1, type: 'green', letter: 'B', 'id': 16},
-  {value: 1, type: 'orange', letter: 'C', 'id': 17},
-  {value: 2, type: 'gold', letter: null, 'id': 18},
+  {value: 1, type: 'red', letter: 'A'},
+  {value: 1, type: 'red', letter: 'B'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 1, type: 'red', letter: 'C'},
+  {value: 2, type: 'blue', letter: 'B'},
+  {value: 1, type: 'brown', letter: 'A'},
+  {value: 1, type: 'green', letter: 'B'},
+  {value: 1, type: 'orange', letter: 'C'},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
+  {value: 2, type: 'gold', letter: null},
 ]
+
+var id = 0;
+for (var card in cards) {
+  cards[card]['id'] = id++;
+}
 
 var gameView = React.renderComponent(
   <GameView/>,
