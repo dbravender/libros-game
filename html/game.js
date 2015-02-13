5a9d8794d7ca89556250304da4d0124259c97119
Dan Bravender <dan.bravender@gmail.com>
2015-02-13 09:43:45 +0900

    Split the view by card type

--- html/game.js
+++ html/game.js
@@ -1,7 +1,28 @@
 /** @jsx React.DOM */
+'use strict';
+
+var GameView = React.createClass({
+  render: function () {
+    var cardsByType = {};
+    for (var card in this.props.cards) {
+      var card = this.props.cards[card];
+      var type = card.type;
+      if (!cardsByType[type]) {
+        cardsByType[type] = [];
+      }
+      cardsByType[type].push(card);
+    }
+    var childNodes = [];
+    for (var type in cardsByType) {
+      var cards = cardsByType[type];
+      childNodes.push(<li key={type}><CardList cards={cards}/></li>);
+    }
+    return <ul>{childNodes}</ul>;
+  }
+});
 
 var Card = React.createClass({
-  getInitialState: function() {
+  getInitialState: function () {
     return {selected: false};
   },
   render: function () {
@@ -10,23 +31,20 @@ var Card = React.createClass({
 });
 
 var CardList = React.createClass({
-  getInitialState: function() {
-    return {cards: []};
-  },
-  render: function() {
+  render: function () {
     var self = this;
-    var totalValue = this.state.cards.reduce(function(x, y) {
+    var totalValue = this.props.cards.reduce(function(x, y) {
       return x + y.value;
     }, 0);
-    var lowestLetter = this.state.cards.reduce(function(x, y) {
+    var lowestLetter = this.props.cards.reduce(function(x, y) {
       return [x, y.letter].sort()[0];
     }, null);
-    var cardNodes = this.state.cards.map(function(card) {
-      return <Card card={card} />;
+    var cardNodes = this.props.cards.map(function(card) {
+      return <Card key={card.id} card={card} />;
     });
     return (
       <div className="card-list">
-        <span class="badge">{totalValue} {lowestLetter}</span>
+        <span className="badge">{totalValue} {lowestLetter}</span>
         <div>{cardNodes}</div>
       </div>
     );
@@ -34,14 +52,14 @@ var CardList = React.createClass({
 });
 
 var cards = [
-  {value: 1, color: 'red', letter: 'A', 'id': 0},
-  {value: 1, color: 'red', letter: 'B', 'id': 1},
-  {value: 1, color: 'red', letter: 'C', 'id': 2},
-  {value: 2, color: 'blue', letter: 'B', 'id': 3}
+  {value: 1, type: 'red', letter: 'A', 'id': 0},
+  {value: 1, type: 'red', letter: 'B', 'id': 1},
+  {value: 1, type: 'red', letter: 'C', 'id': 2},
+  {value: 2, type: 'blue', letter: 'B', 'id': 3}
 ]
 
-var cardList = React.renderComponent(
-  <CardList/>,
+var gameView = React.renderComponent(
+  <GameView/>,
   document.getElementById('card-list')
 );
-cardList.setState({cards: cards});
+gameView.setProps({cards: cards});
